6c8761b533d79734fcfda8853ea2ba3fa7cdf5c6
Jerin Jaison <jerinjaison07@gmail.com>
2026-02-23 23:55:34 +0530

    Safely import debug_toolbar in urls.py

--- sahasrara/urls.py
+++ sahasrara/urls.py
@@ -23,7 +23,10 @@ handler500 = 'apps.pages.views.error_500'
 handler403 = 'apps.pages.views.error_403'
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
+    try:
+        import debug_toolbar
+        urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
+    except ImportError:
+        pass
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
